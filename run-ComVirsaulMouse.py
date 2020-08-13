f5be382ca845960c8fbb443fd5ea7998997e4302
opopopopopop <onepiece14515@gmail.com>
2020-08-13 09:13:40 +0700

    running program
    -(completed following requirement) run a mini rectangle game
    -(starting) run computer virsual mouse
    -(completed) run read write for getting finger distance
    -(completed) run distance histogram for getting the hand data

--- run-ComVirsaulMouse.py
+++ run-ComVirsaulMouse.py
@@ -1,16 +1,13 @@
 import cv2
 import numpy as np
 import random
+import os
+import pyautogui
 from src.hand_tracker import HandTracker
 from scipy.interpolate import interp1d
 from numpy import interp
 from scipy.spatial import distance
-import glob
 from PIL import Image
-import os
-import pandas as pd
-from matplotlib import pyplot as plt
-import ast
 
 
 WINDOW = "Hand Tracking"
@@ -18,7 +15,6 @@ WINDOW2 = "Rectangle Game"
 PALM_MODEL_PATH = "models/palm_detection_builtin.tflite"
 LANDMARK_MODEL_PATH = "models/hand_landmark.tflite"
 ANCHORS_PATH = "models/anchors.csv"
-IMAGES_PATH = "W:/mockup-handDetection/mockup-handdetection-mediapipe/imagesGestRecog3/rock/*"
 
 POINT_COLOR = (0, 255, 0)
 CONNECTION_COLOR = (255, 0, 0)
@@ -26,11 +22,14 @@ THICKNESS = 2
 
 
 
-def gesture(INDEXFINGER, MIDDLEFINGER, RINGFINGER, LITTLEFINGER,frame):
+def gestureClick(MIDDLEFINGER, RINGFINGER, LITTLEFINGER,frame):
+    isClick = False
     if not MIDDLEFINGER and not RINGFINGER and not LITTLEFINGER:
         cv2.putText(frame, "Close", (10,20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
+        return True
     if MIDDLEFINGER and RINGFINGER and LITTLEFINGER:
         cv2.putText(frame, "Open", (10,20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
+    return isClick
 
 def finger_state(points):
     # finger state
@@ -129,15 +128,14 @@ def fingerState_distance_ratio(points):
     distanceFinger.append(distance.euclidean(points[0], points[20])/distance.euclidean(points[0], points[17]))
     if distance.euclidean(points[0], points[20])/distance.euclidean(points[0], points[17]) > 1 :
         LITTLEFINGER = True
-    # return INDEXFINGER, MIDDLEFINGER, RINGFINGER, LITTLEFINGER, distanceFinger
-    return distanceFinger
-def xyrandom():
-    x = random.randint(0, 380)
-    y = random.randint(0, 540)
+    return INDEXFINGER, MIDDLEFINGER, RINGFINGER, LITTLEFINGER, distanceFinger
+    # return distanceFinger
+def xyrandom(xMax,yMax):
+    x = random.randint(0, xMax)
+    y = random.randint(0, yMax)
     return x, y
 
-def randomRectangle(framee):
-    x, y = xyrandom()
+def randomRectangle(framee,x,y):
     w = x+100
     h = y+100
     cv2.rectangle( framee,(x, y),(w,h),(255,0,0), 1, 8)
@@ -149,78 +147,99 @@ def isHandInRectngle(xP,yP,x,y,w,h):
     else:
         return False
 
-def mappingPointX(frameCamera,displayScreen,pointX):
+def mappingPointX(frameCamera,wN,pointX):
     wO = frameCamera.shape[1]
-    wN = displayScreen.shape[1]
     # mX = interp1d([0,wO],[0,wN])
     # return int(mX(pointX))
     return int(interp(pointX,[0,wO],[0,wN]))
 
 
-def mappingPointY(frameCamera,displayScreen,pointY):
+def mappingPointY(frameCamera,hN,pointY):
     hO = frameCamera.shape[0]
-    hN = displayScreen.shape[0]
     # mY = interp1d([0,hO],[0,hN])
     # return int(mY(pointY))
     return int(interp(pointY,[0,hO],[0,hN]))
 
-# print("Frame shape: "+ str(frame.shape))
-
-# height = frame.shape[0]
-# width = frame.shape[1]
-# channels = frame.shape[2]
-
-# read open-close distance text file
-allDistanceFinger = []
-fileRead = open("images/closeHand-Dis.txt", "r")
-fileLines = fileRead.readlines()
-if fileRead.mode == 'r':
-    for readBuffer in fileLines:
-        readBuffer = ast.literal_eval(readBuffer)
-        print(readBuffer)
-        allDistanceFinger.append(readBuffer)
-print(allDistanceFinger)
-
-# Calculate educlidiun distance index middle ring little
-# images = glob.glob(IMAGES_PATH)
-
-## writning open/close hand file
-# f= open("closeHand-Dis.txt","w+")
-# for image in images:
-#     img = cv2.imread(image)
-#     img_rotate_90_clockwise = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
-#     image = cv2.cvtColor(img_rotate_90_clockwise, cv2.COLOR_BGR2RGB)
-
-#     # image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     points, _ = detector(image)
-#     if points is not None:
-#         # THUMB, INDEXFINGER, MIDDLEFINGER, RINGFINGER, LITTLEFINGER = finger_state(points)
-#         distanceFinger = fingerState_distance_ratio(points)
-#         # gesture(INDEXFINGER, MIDDLEFINGER, RINGFINGER, LITTLEFINGER,display)
-#         f.write(str(distanceFinger)+"\n")
-#         allDistanceFinger.append(distanceFinger)
-# f.close()
 
 
+cv2.namedWindow(WINDOW)
+capture = cv2.VideoCapture(0)
 
-# df = pd.DataFrame(allDistanceFinger, columns=['index', 'middle', 'ring','little'])
-# print(df.describe())
-# boxplot = df.boxplot(column=['index', 'middle', 'ring','little'])
-# boxplot
-# colors = ['red', 'tan', 'lime','blue']
-# labels= ['index', 'middle', 'ring','little']
+if capture.isOpened():
+    hasFrame, frame = capture.read()
+    frame = cv2.resize(frame, (1920, 1080))
+    hasFrame = cv2.resize(hasFrame, (1920, 1080))
+else:
+    hasFrame = False
 
-# kwargs = dict(alpha=0.5, bins=100,histtype='bar')
+connections = [
+    (0, 1), (1, 2), (2, 3), (3, 4),
+    (5, 6), (6, 7), (7, 8),
+    (9, 10), (10, 11), (11, 12),
+    (13, 14), (14, 15), (15, 16),
+    (17, 18), (18, 19), (19, 20),
+    (0, 5), (5, 9), (9, 13), (13, 17), (0, 17)
+]
 
-# plt.hist(df['index'], **kwargs, color='g', label='indexFinger')
-# plt.hist(df['middle'], **kwargs, color='b', label='middleFinger')
-# plt.hist(df['ring'], **kwargs, color='r', label='ringFinger')
-# plt.hist(df['little'], **kwargs, color='y', label='littleFinger')
+detector = HandTracker(
+    PALM_MODEL_PATH,
+    LANDMARK_MODEL_PATH,
+    ANCHORS_PATH,
+    box_shift=0.2,
+    box_enlarge=1.3
+)
+comShape = pyautogui.size()
+print("Frame shape: "+ str(frame.shape))
+print(comShape)
 
-# plt.hist([df['index'],df['middle'],df['ring'],df['little']],10,density=True,histtype='bar',
-# color=colors,label=labels)
-# plt.legend()
-# plt.show()
+channels = frame.shape[2]
 
-# capture.release()
-# cv2.destroyAllWindows()
+heightCom = comShape[1]
+widthCom = comShape[0]
+
+# height = pyautogui.size()['weidth']
+# width = frame.shape()
+
+while hasFrame:
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    points, _ = detector(image)
+    frame = cv2.resize(frame, (widthCom, heightCom))
+    if points is not None:
+        newPoints = []
+        for point in points:
+            x, y = point
+            cv2.circle(frame, (int(x), int(y)), THICKNESS * 2, POINT_COLOR, THICKNESS)
+            # cv2.putText(frame, str(int(x))+","+str(int(y)), (int(x)+4, int(y)+2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+        # print(str(distance.euclidean(points[5], points[8]))+" , "+str(distance.euclidean(points[0], points[8])))
+
+        # mapping new xy point of Computer
+        for point in points:
+            x, y = point
+            newPoint = []
+            newX = mappingPointX(frame,widthCom,x)
+            newY = mappingPointY(frame,heightCom,y)
+            newPoint.append(newX)
+            newPoint.append(newY)
+            newPoints.append(newPoint)
+
+        # track the first finger points[8]
+        xP, yP = newPoints[0]
+        # pyautogui.moveTo(xP, yP, duration = 1)
+
+        INDEXFINGER, MIDDLEFINGER, RINGFINGER, LITTLEFINGER, distanceFinger = fingerState_distance_ratio(points)
+
+        # connection camera screen
+        for connection in connections:
+            x0, y0 = points[connection[0]]
+            x1, y1 = points[connection[1]]
+            cv2.line(frame, (int(x0), int(y0)), (int(x1), int(y1)), CONNECTION_COLOR, THICKNESS)
+
+    cv2.imshow(WINDOW, frame)
+    hasFrame, frame = capture.read()
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
